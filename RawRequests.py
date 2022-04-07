696ef0b9679e3085b511da7ede6a800f5e4bb9c4
rafax00 <rafa.santos025@gmail.com>
2022-04-07 09:46:09 -0300

    Lib

---
+++ RawRequests.py
@@ -0,0 +1,160 @@
+import socket
+import RequestUtils
+import ssl
+import sys
+import gzip
+
+line_separator = "\r\n"
+
+def exception(message, function):
+    message = str(function) + " => " + str(message)
+    print(message)
+
+def check_ssl(scheme):
+    try:
+        if scheme == "https://":
+            return True
+        else:
+            return False
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def send(url, method, headers, body, timeout):
+    try:
+        host = RequestUtils.get_host_from_url(url)
+        port = RequestUtils.get_url_port(url)
+        raw_request = build_request(url, method, headers, body, host)
+
+        use_ssl = check_ssl(RequestUtils.get_scheme_from_url(url))
+
+        response = send_raw(raw_request, port, host, timeout, use_ssl)
+
+        if response is not None:
+            return response
+            res = make_object(response)
+            if res.status_code == 0:
+                res = response
+        else:
+            res = None
+
+        return res
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+
+def gzip_decode(data):
+    try:
+        body = str(data[data.find('\r\n\r\n')+4:])
+        try:
+            decoded_body = str(gzip.decompress(bytes(body.encode())), "latin1")
+
+            return decoded_body
+        except Exception as error:
+            pass
+
+        return body
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def send_raw(raw_request, port, host, timeout, use_ssl):
+    try:
+        if use_ssl:
+            context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+            context.verify_mode = ssl.CERT_REQUIRED
+            context.check_hostname = True
+            context.load_default_certs()
+
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            w_socket = context.wrap_socket(s, server_hostname=host)
+            w_socket.settimeout(timeout)
+            w_socket.connect((host, int(port)))
+        else:
+            w_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            w_socket.settimeout(timeout)
+            w_socket.connect((host, int(port)))
+
+        w_socket.send(bytes(raw_request, encoding="latin1"))
+        data = w_socket.recv(4096).decode("latin1")
+        w_socket.close()
+
+        return data
+    except Exception as error:
+        if "Name or service not known" or "UnicodeError" in str(error):
+            return None
+        exception(host + str(error), sys._getframe().f_code.co_name)
+        return None
+
+def make_object(raw_response):
+    try:
+        class response():
+            status_code = 0
+            text = ""
+            header = ""
+            raw = ""
+
+        splited_response = raw_response.split(line_separator + line_separator)
+
+        headers = splited_response[0]
+        if len(splited_response) > 1:
+            text = splited_response[1]
+        else:
+            text = ""
+        splited_headers = headers.split(" ")
+
+        status_code = 0
+        if len(splited_headers) > 1:
+            status_code = int(splited_headers[1])
+
+        response.status_code = status_code
+        response.raw = raw_response
+        response.text = text
+        response.header = headers
+
+        return response
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+
+def build_request(url, method, headers, body, host):
+    try:
+        has_host = False
+        has_connection = False
+        has_content_length = False
+        has_useragent = False
+
+        path = RequestUtils.get_path_from_url(url)
+
+        raw_request = method.upper() + " " + path + " HTTP/1.1" + line_separator
+
+        for key, value in headers.items():
+            if key.lower() == "host":
+                has_host = True
+            if "content-length" in key.lower():
+                has_content_length = True
+            if key.lower() == "connection":
+                has_connection = True
+            if key.lower() == "user-agent":
+                has_useragent = True
+
+            raw_request += key + ": " + value + line_separator
+
+        if not has_host:
+            raw_request += "Host: " + host + line_separator
+
+        if not has_connection:
+            raw_request += "Connection: close\r\n"
+
+        if not has_useragent:
+            raw_request += "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36\r\n"     
+
+        if len(body) > 0:
+            if not has_content_length:
+                raw_request += "Content-Length: " + str(RequestUtils.calculate_content_lentgh(body)) + line_separator
+            raw_request += line_separator
+            raw_request += body
+        else:
+            raw_request += line_separator
+
+        return raw_request
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
---
+++ RequestUtils.py
@@ -0,0 +1,137 @@
+
+import requests
+import urllib3
+import sys
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+public_session = requests.Session()
+
+def exception(message, function):
+    message = str(function) + " => " + str(message)
+    print(message)
+
+def calculate_content_lentgh(body):
+    try:
+        return len(body)
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def get_url_port(url):
+    try:
+        scheme = get_scheme_from_url(url)
+        splited_host = url.split(scheme)[1].split("/")[0].split("?")[0].split("#")[0].split(":")
+        port = ""
+
+        if "https://" == scheme:
+            port = "443"
+        elif "http://" == scheme:
+            port = "80"
+
+        if len(splited_host) > 1:
+            port = splited_host[1]
+        
+        return port
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def get_path_folder_from_url(url):
+    try:
+        folder = url.rsplit('/',1)
+        
+        return folder[0] + "/"
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def get_string_port_from_url(url):
+    try:
+        base = url.replace(get_scheme_from_url(url), "").split("?")[0].split("/")[0].split("#")[0]
+        if ":" in base:
+            return ":" + base.split(":")[1]
+        else:
+            return ""
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def get_path_from_url(url):
+    try:
+        host = get_host_from_url(url)
+        scheme = get_scheme_from_url(url)
+        string_port = get_string_port_from_url(url)
+
+        url_base = scheme + host + string_port
+        path = "/"
+        splited_url = url.split(url_base)
+        if len(splited_url) > 1:
+            path = splited_url[1]
+        if path == "":
+            path = "/"
+        return path
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def get_host_from_url(url):
+    try:
+        host = url.split("://")[1].split("/")[0].split("?")[0].split("#")[0].split(":")[0]
+
+        return host
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+        return "www.google.com"
+
+def calculate_encoded_data(data):
+    data = data.replace("\r\n", "rc")
+    return str(hex(len(data)))[2:]
+
+def make_request_public_session(url, method, headers, body):
+    try:
+        if len(body) > 0:
+            prepped = requests.Request(method, url, data=body, headers=headers).prepare()
+        else:
+            prepped = requests.Request(method, url, headers=headers).prepare()
+
+        req = public_session.send(prepped, verify=False, timeout=5, allow_redirects=False)
+
+        return req
+    except Exception as error:
+        pass
+
+def get_scheme_from_url(url):
+    try:
+        return url.split("://")[0] + "://"
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
+
+def make_request_unique_session(url, method, headers, body):
+    try:
+        private_session = requests.Session()
+        if len(body) > 0:
+            prepped = requests.Request(method, url, data=body, headers=headers).prepare()
+        else:
+            prepped = requests.Request(method, url, headers=headers).prepare()
+
+        prepped.headers["Content-length"] = headers["Content-Length"]
+        req = private_session.send(prepped, verify=False, timeout=10, allow_redirects=False)
+
+        return req
+    except Exception as error:
+        pass
+
+def create_raw_request(url, method, headers, body):
+    try:
+        has_host = False
+        raw_request = method + " " + get_path_from_url(url) + " HTTP/1.1\r\n"
+        for key, value in headers.items():
+            if key.lower() == "host":
+                has_host = True
+            raw_request += key + ": " + value + "\r\n"
+
+        if not has_host:
+            raw_request += "Host: " + get_host_from_url(url) + "\r\n"
+
+        raw_request += "\r\n"
+
+        raw_request += body
+
+        return raw_request
+    except Exception as error:
+        exception(error, sys._getframe().f_code.co_name)
